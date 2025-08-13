8bfc7e72351d394baa81a974ebce37f20408ce79
Stromenko <nikita.stromenko@tafs.pro>
2025-08-13 19:28:14 +0300

    Add first version of ini parser

--- src/formats/ini.rs
+++ src/formats/ini.rs
@@ -1,10 +1,24 @@
-pub fn parse_line(line: &str) -> Option<&str> {
-    let line = line.trim();
-    if line.starts_with('[') && line.ends_with(']') && line.len() >= 2 {
-        return Some(&line[1..line.len() - 1]);
+use std::collections::HashMap;
+use std::fs;
+
+pub fn parse_file(filename: &str) -> Option<HashMap<String, HashMap<String, String>>> {
+    let mut map = HashMap::<String, HashMap<String, String>>::new();
+
+    let mut segment = String::new();
+    fs::read_to_string(filename).unwrap().lines().map(|l| l.trim()).for_each(|line| {
+        if line.starts_with('[') && line.ends_with(']') && line.len() >= 2 {
+            segment = (&line[1..line.len() - 1]).to_string();
+            map.insert(segment.to_string(), HashMap::<String, String>::new());
+        }
+        if let Some((key, value)) = line.split_once('=') {
+            if map.contains_key(&segment) {
+                map.get_mut(&segment).unwrap().insert(key.trim().to_string(), value.trim().to_string());
+            }
+        }
+    });
+
+    if map.is_empty() {
+        return None
     }
-    if let Some((key, value)) = line.split_once('=') {
-        println!("Key: '{}' value: '{}'", key.trim(), value.trim());
-    }
-    None
+    Some(map)
 }
--- src/reader.rs
+++ src/reader.rs
@@ -1,24 +1,19 @@
-use std::fs;
+use std::collections::HashMap;
 use std::path::Path;
 
 use crate::formats::ini;
 
-pub fn read_lines(filename: &str){
-    let file_type;
+pub fn read_lines(filename: &str) {
+    let result: Option<HashMap<String, HashMap<String, String>>>;
     match parse_file_type(filename).as_str() {
-        "ini" => file_type = "ini".to_string(),
-        "json" => file_type = "json".to_string(),
-        "toml" => file_type = "toml".to_string(),
-        _ => file_type = "unknown".to_string()
+        "ini" => {
+            result = ini::parse_file(filename);
+            println!("{:?}", result);
+        },
+        "json" => {},
+        "toml" => {},
+        _ => {}
     }
-    println!("{}", file_type);
-    fs::read_to_string(filename).unwrap().lines().for_each(|line| {
-        let res = ini::parse_line(line);
-        match res {
-            Some(_) => println!("{}", res.unwrap()),
-            None => ()
-        }
-    });
 }
 
 fn parse_file_type(filename: &str) -> String {
